657b08ac2a53fbd3cd9a9577f7d6f2efe1da3657
Tomas Vojtech <tomas.vojtech@hpe.com>
2017-02-20 15:51:57 +0100

    styles, breadcrumbs, language switching

--- src/main.controller.js
+++ src/main.controller.js
@@ -1,4 +1,8 @@
-angular.module('notebook').controller('MainController', function ($translate) {
+angular.module('notebook').controller('MainController', function ($translate, $uibModal) {
     this.changeLang = lang => $translate.use(lang)
     this.isCurrentLang = lang => ($translate.use() || '').indexOf(lang) !== -1
+    this.openCreateNote = () => $uibModal.open({
+        bindToController: true,
+        
+    })
 })
