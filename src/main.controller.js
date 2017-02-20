8063ae2c098891537f91366678e20165d9de6aab
Tomas Vojtech <tomas.vojtech@hpe.com>
2017-02-20 16:43:50 +0100

    create note

--- src/main.controller.js
+++ src/main.controller.js
@@ -1,8 +1,5 @@
 angular.module('notebook').controller('MainController', function ($translate, $uibModal) {
     this.changeLang = lang => $translate.use(lang)
     this.isCurrentLang = lang => ($translate.use() || '').indexOf(lang) !== -1
-    this.openCreateNote = () => $uibModal.open({
-        bindToController: true,
-        
-    })
+    this.openCreateNote = () => $uibModal.open({component: 'createNote'})
 })
