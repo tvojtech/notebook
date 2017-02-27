86fafbe9335aef58dc37b2f1fcfdeb814949a40a
Tomas Vojtech <tomas.vojtech@gmail.com>
2017-02-27 01:30:51 +0100

    build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const nodemon = require('gulp-nodemon')
 const concat = require('gulp-concat')
 const del = require('del')
 const runSequence = require('run-sequence')
-const watch = require('gulp-watch')
 const babel = require('gulp-babel')
 const sass = require('gulp-sass')
 const docker = require('gulp-docker')
@@ -13,12 +12,17 @@ const angularProtractor = require('gulp-angular-protractor')
 const inject = require('gulp-inject')
 const bowerFiles = require('main-bower-files')
 const path = require('path')
+const ngmin = require('gulp-ngmin')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const htmlmin = () => require('gulp-htmlmin')({removeComments: true, collapseWhitespace: true})
+const jsonMinify = require('gulp-json-minify')
 
 const tempDest = '.tmp'
+const distDest = 'dist'
 const jsUiSources = ['src/**/*.js']
 const jsApiSources = ['api/**/*.js']
 const readJsSources = () => gulp.src(jsUiSources.concat(jsApiSources))
-const readUiSources = () => gulp.src(jsUiSources.concat(['!bower_components/**/*.js ']))
 const e2eTestSources = () => gulp.src(['e2e/**/*.test.js'])
 
 gulp.task('clean', () => {
@@ -32,20 +36,6 @@ gulp.task('server-dev', () => {
     watch: ['api/**/*.js'],
     ext: 'js'
   })
-})
-
-
-gulp.task('concat', function () {
-  gulp.src('src/**/*.js').pipe(concat('main.js')).on('error', console.log).pipe(gulp.dest('dist'))
-})
-
-gulp.task('docker', () => {
-  new docker(gulp, {
-    sidekick: {
-      dockerfile: '.'
-    }
-  })
-  gulp.start('docker:image')
 })
 
 gulp.task('eslint', () => readJsSources().pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError()))
@@ -62,7 +52,7 @@ gulp.task('test:e2e', done => {
     .on('end', done)
 })
 
-gulp.test('test')
+gulp.task('test')
 
 gulp.task('copy-public', () => gulp.src('public/**/*').pipe(gulp.dest(tempDest)))
 gulp.task('build-public', ['copy-public'], () => {
@@ -80,25 +70,67 @@ gulp.task('build-locales', () => gulp.src('locales/**/*').pipe(gulp.dest(path.jo
 gulp.task('build-bower', () => gulp.src('bower_components/**/*').pipe(gulp.dest(path.join(tempDest, 'bower_components'))))
 gulp.task('build-sass', () => gulp.src('src/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('.tmp')))
 
-gulp.task('build-dev', () => runSequence(
+gulp.task('build-dev', done => runSequence(
   ['build-js', 'build-html', 'build-locales', 'build-sass', 'build-bower'],
-  'build-public'
+  'build-public',
+  done
   )
 )
 
-gulp.task('watch-public', () => watch('public/**/*', runSequence('build-public')))
-gulp.task('watch-js', () => watch('src/**/*.js', runSequence('build-js')))
-gulp.task('watch-html', () => watch('src/**/*.html', runSequence('build-html')))
-gulp.task('watch-locales', () => watch('locales/**/*', runSequence('build-locales')))
-gulp.task('watch-bower', () => watch('bower_components/**/*', runSequence('build-bower')))
-gulp.task('watch-sass', () => watch('src/**/*.scss', runSequence('build-sass')))
+gulp.task('watch-public', () => gulp.watch('public/**/*', ['build-public']))
+gulp.task('watch-js', () => gulp.watch('src/**/*.js', ['build-js']))
+gulp.task('watch-html', () => gulp.watch('src/**/*.html', ['build-html']))
+gulp.task('watch-locales', () => gulp.watch('locales/**/*', ['build-locales']))
+gulp.task('watch-bower', () => gulp.watch('bower_components/**/*', ['build-bower']))
+gulp.task('watch-sass', () => gulp.watch('src/**/*.scss', ['build-sass']))
 
 gulp.task('watch', ['watch-public', 'watch-js', 'watch-html', 'watch-locales', 'watch-bower', 'watch-sass'])
 
-gulp.task('build', () => {
-  runSequence(['clean', 'eslint', 'test'], 'build-dev', 'concat', 'docker')
+gulp.task('build-html-prod', () => gulp.src(path.join(tempDest, '**/*.html')).pipe(htmlmin()).pipe(gulp.dest(distDest)))
+gulp.task('build-js-prod', ['build-html-prod'], () =>
+  // todo: add templates to template cache
+  gulp.src(path.join(tempDest, '**/*.js'))
+    .pipe(concat('bundle.js')).on('error', console.log)
+    .pipe(ngmin())
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest(distDest))
+)
+gulp.task('build-css-prod', () =>
+  gulp.src(path.join(tempDest, '/**/*.css'))
+    .pipe(concat('app.css'))
+    .on('error', console.log)
+    .pipe(uglifycss())
+    .pipe(gulp.dest(distDest))
+)
+gulp.task('build-locales-prod', () => gulp.src(path.join(tempDest, 'locales/**/*')).pipe(jsonMinify()).pipe(gulp.dest(path.join(distDest, 'locales'))))
+gulp.task('copy-public-prod', () => gulp.src('public/**/*').pipe(gulp.dest(distDest)))
+gulp.task('build-index-prod', () =>
+  gulp.src('index.html', {cwd: 'public'})
+    .pipe(inject(gulp.src(['bundle.js', 'app.css'], {
+      read: false,
+      cwd: distDest
+    }), {addRootSlash: false, relative: true, ignorePath: '../dist'}))
+    .pipe(htmlmin())
+    .pipe(gulp.dest(distDest))
+)
+
+gulp.task('build-prod', done => runSequence(
+  ['build-js-prod', 'build-css-prod', 'build-locales-prod'],
+  'copy-public-prod',
+  'build-index-prod',
+  done
+  )
+)
+
+gulp.task('docker', () => {
+  new docker(gulp, {
+    sidekick: {
+      dockerfile: '.'
+    }
+  })
+  gulp.start('docker:image')
 })
 
-gulp.task('default', done => {
-  runSequence(['clean', 'eslint'], 'build-dev', 'server-dev', done)
-})
+gulp.task('build', done => runSequence(['clean', 'eslint', 'test'], 'build-dev', 'build-prod', done))
+
+gulp.task('default', done => runSequence(['clean', 'eslint'], 'build-dev', ['server-dev', 'watch'], done))
