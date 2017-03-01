a7997e95ef6acc047ae84467cfdd0a469d3c931f
Tomas Vojtech <tomas.vojtech@gmail.com>
2017-03-01 23:55:23 +0100

    fix: fix test

--- e2e/list.test.js
+++ e2e/list.test.js
@@ -5,7 +5,8 @@ describe('note list', function () {
     element(by.model('$ctrl.search')).sendKeys('Jogging')
 
     let noteList = element.all(by.css('.list-group-item'))
-    expect(noteList.count()).toEqual(1)
+    // searching is delegated to server and apiary mock always returns 2 items
+    expect(noteList.count()).toEqual(2)
     expect(noteList.get(0).getText()).toEqual('Jogging in park')
 
     element(by.model('$ctrl.search')).clear()
